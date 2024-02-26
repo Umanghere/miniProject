67676e8135b539f54dc1cb90000bab71238cc2ec
Umang Bansal <126900849+Umanghere@users.noreply.github.com>
2024-02-26 22:16:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// let todo=[];
+// let choice= prompt("Enter you Choice; ");
+// while( (choice != "quit") && (choice != "Quit") ){
+//     if(choice == "list"){
+//         for(let i=0; i<length.choice; i++){
+//             console.log(i, todo);
+//         }
+//     }
+// }
